8fe6c9f52e517f7f75d59eef34d61799a9543f98
Qiyas <justerfeedy@gmail.com>
2024-09-15 11:59:15 +0300

    removed unused component EstimateAttackCooldownTime

--- Assets/Project/Runtime/ECS/Components/CombatComponents.cs
+++ Assets/Project/Runtime/ECS/Components/CombatComponents.cs
@@ -88,14 +88,6 @@ namespace Project.Runtime.ECS.Components
     [Il2CppSetOption(Option.NullChecks, false)]
     [Il2CppSetOption(Option.ArrayBoundsChecks, false)]
     [Il2CppSetOption(Option.DivideByZeroChecks, false)]
-    public struct EstimateAttackCooldownTime : IComponent
-    {
-        public float Value;
-    }
-
-    [Il2CppSetOption(Option.NullChecks, false)]
-    [Il2CppSetOption(Option.ArrayBoundsChecks, false)]
-    [Il2CppSetOption(Option.DivideByZeroChecks, false)]
     public struct HealthbarTag : IComponent
     {
     }
